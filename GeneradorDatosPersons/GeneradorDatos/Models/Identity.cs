dfd601d3a0083ba3f30e1b4b7cce545df1c2ae5c
Daniela Melendres Villarreal <a2017113441@estudiantes.upsa.edu.bo>
2021-03-08 09:25:58 -0400

    Ejercicio completo

---
+++ GeneradorDatosPersons/GeneradorDatos/Models/Identity.cs
@@ -0,0 +1,12 @@
+﻿namespace GeneradorDatos.Models
+{
+    public class Identity
+    {
+        public int Identityid { get; set; }
+        public string Name { get; set; }
+        public string Photo { get; set; }
+        public string Sex { get; set; }
+
+
+    }
+}
